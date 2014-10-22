bf6b31cb91c08b3a20d3c42f7de97eb7ebe0d0d6
Alex Chi <alex@alexchi.me>
2014-10-22 18:27:46 +0800

    a tiny revise

--- build/travis/to_after_success.sh
+++ build/travis/to_after_success.sh
@@ -1,3 +1,11 @@
-./out/bin/t001
-coveralls --exclude build/ --exclude data/ --exclude doc/ --exclude code/external/ --exclude out/make/external/ --exclude out/make/CMakeFiles/ --gcov-options '\-lp'
+echo ">>> after success"
+
+if [ "$RUN_COVERALLS"x = "true"x ]; then
+  ./out/bin/t001
+  coveralls --exclude build/ --exclude data/ --exclude doc/ --exclude code/external/ --exclude out/make/external/ --exclude out/make/CMakeFiles/ --gcov-options '\-lp'
+else
+  echo "< don't run anything for coveralls >"
+fi
 #valgrind --leak-check=yes out/bin/test
+
+echo "<<< after success"
