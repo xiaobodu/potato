bf50e3c629a7951ed62e73e4ca752ee9921ab9f3
Alex Chi <alex@alexchi.me>
2014-10-24 11:33:00 +0800

    ignore some builds in travis-ci

--- build/travis/to_before_install.sh
+++ build/travis/to_before_install.sh
@@ -1,2 +1,11 @@
+echo ">>> before install"
+if [ -z $TRAVIS_TAG ]; then
+if [ "$RUN_COVERALLS"x = "true"x ]; then
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update
+fi
+elif [ "$DEPLOY_RELEASE"x = "true"x ]; then
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo apt-get update
+fi
+echo "<<< before install"
