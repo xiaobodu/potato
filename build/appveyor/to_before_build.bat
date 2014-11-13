b21caa7463e4e650f822bc832590bc7a62fb98c7
Alex Chi <alex@alexchi.me>
2014-11-13 21:40:13 +0800

    fix the appveyor error

--- build/appveyor/to_before_build.bat
+++ build/appveyor/to_before_build.bat
@@ -14,7 +14,7 @@ cd ../libpng/
 echo "||| generate the libpng solution"
 cmake -G "Visual Studio 11" -D PNG_DEBUG=FALSE -D PNG_SHARED=FALSE -D PNG_TESTS=FALSE -D ZLIB_INCLUDE_DIR=code/external/zlib -D ZLIB_LIBRARY=../../code/external/zlib/Release/zlibstatic.lib ./
 cd ../../../build/
-config-tcc.bat
+call config-tcc.bat
 cd ../out/msbuild/
 echo "||| generate the potato solution"
 cmake -G "Visual Studio 11" -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_CXX_COMPILER_ID=MSVC -D ZLIB_INCLUDE_DIR=code/external/zlib -D ZLIB_LIBRARY=../../code/external/zlib/Release/zlibstatic.lib ../../build/cmake
