044d3ae0bb3ad5de2c1c9855e29d494d54057aea
Alex Chi <alex@alexchi.me>
2014-11-15 11:32:26 +0800

    update the android

---
+++ build/make-android-release.sh
@@ -0,0 +1,6 @@
+cd android/libpotato/jni/
+#ndk-build NDK_PROJECT_PATH=./out/android/ NDK_APPLICATION_MK=./build/android/jni/Application.mk APP_BUILD_SCRIPT=./../../build/android/jni/Android.mk $1
+ndk-build NDK_MODULE_PATH=`pwd` $1
+cd ../../test/jni/
+ndk-build NDK_MODULE_PATH=`pwd` $1
+cd ../../
