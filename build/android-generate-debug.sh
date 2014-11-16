164ada08389f0cea36e8671869028e7f07371ade
Alex Chi <alex@alexchi.me>
2014-11-16 23:26:21 +0800

    update again

---
+++ build/android-generate-debug.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+cd $(dirname $0)
+
+export PATH=$PATH:$ANDROID_HOME:$NDK_HOME
+
+cd ./android/test/
+ant clean
+./../../android-make-debug.sh clean
+./../../android-make-debug.sh
+ant debug
+cd ../../
