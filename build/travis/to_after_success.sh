aa574be8262f98099b9b657e03f175bb98df78f2
Alex Chi <alex@alexchi.me>
2015-01-10 09:15:36 +0800

    try to fix an error about the coveralls again

--- build/travis/to_after_success.sh
+++ build/travis/to_after_success.sh
@@ -20,7 +20,7 @@ fi
 set -e
 
 if [ "$RUN_COVERALLS"x = "true"x ]; then
-  sudo ln -s /usr/bin/gcov-4.8 /usr/bin/gcov
+  sudo ln -s -f /usr/bin/gcov-4.8 /usr/bin/gcov
   build/linux-run-demo.sh
   coveralls --exclude build/ --exclude data/ --exclude doc/ --exclude code/external/ --exclude out/make/external/ --exclude out/make/CMakeFiles/ --gcov-options '\-lp'
 else
